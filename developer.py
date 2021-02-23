592ff5014844aab62fedf5cc99acf6d165d75711
misaka10843 <69132853+misaka10843@users.noreply.github.com>
2021-02-23 20:47:02 +0800

    Add files via upload

---
+++ developer.py
@@ -0,0 +1,61 @@
+# coding:utf-8
+# 2021-2-23
+#TapTap厂商评论存json爬虫
+import requests
+import csv
+import time
+import pandas
+from bs4 import BeautifulSoup
+import re
+import json
+
+
+def get_page(url, headers):
+    data = []
+    url = url
+    headers = headers
+    r = requests.get(url, headers)
+    soup = BeautifulSoup(r.text, "lxml")
+    # file = open("test1.TXT", 'r',encoding='utf-8')
+    for li in soup.find_all(name='li', class_="taptap-review-item collapse in"):
+        user_name = li['data-user']
+        review_id = str(li['id'])[7:]
+        user_id = li.span['data-user-id']
+        score_list = li.find_all(name='i')[0]  # "width:70px"
+        score = str(score_list)[26:-6]
+        revi_ew = []
+        review_list = li.find_all(name='p')  # 抓取评论 <p>列表
+        for i in range(len(review_list)):
+            re = str(review_list[i])[3:-4]
+            revi_ew.append(re)
+        review = " ".join(revi_ew)
+        r = li.find_all(name='span')
+        time = r[3].string
+        device = r[4].string
+        data.append({'user': user_name, 'id': user_id, 'review_id': review_id, 'score':score, 'review':review, 'time':time, 'device':device })
+    return data
+def save_data(data):
+       with open('developer.json', 'a') as fp:  #将所得的数据存储为json文件,developer.json可以改存放路径
+        for i in range(1,2):
+         json.dump(data, fp = fp, ensure_ascii = False, indent = 4, sort_keys = True)
+
+
+def main():
+    for i in range(1, 2):#2可以改成你想爬的页数，实际页数=修改的数字 - 1
+        print("第", i, "页")
+        url = "https://www.taptap.com/developer/2004/review?page="#2004改成你需要爬的id
+        url = url + str(i)
+        headers = {
+            "Accept": 'application / json, text / javascript, * / *; q = 0.01',
+            "Accept - Encoding": 'gzip, deflate, br',
+            "Accept - Language": 'zh - CN, zh;q = 0.9',
+            "Connection": 'keep - alive,',
+            "User - Agent": 'Mozilla / 5.0(Windows NT 10.0;WOW64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / 63.0.3239.132 Safari / 537.36'
+        }
+        time.sleep(4)
+        data = get_page(url, headers)
+        save_data(data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ game.py
@@ -0,0 +1,63 @@
+# coding:utf-8
+# 2021-2-23
+#TapTap游戏评论存json爬虫
+import requests
+import json
+import csv
+import time
+import random
+
+
+headers = {
+    'Host': 'api.taptapdada.com',
+    'Connection': 'Keep-Alive',
+    'Accept-Encoding': 'gzip',
+    'User-Agent': 'okhttp/3.10.0'
+
+}
+
+# 声明一个列表存储字典
+data_list = []
+
+
+def start_spider():
+    page = 0
+    while True:
+        time.sleep(round(random.uniform(0.5, 1.5), 1))
+        url = 'https://api.taptapdada.com/review/v1/by-app?limit=10&app_id=185982' \ #185982可改成你需要爬的id
+              '&X-UA=V%3D1%26PN%3DTapTap%26VN_CODE%3D551%26LOC%3DCN%26LANG%3Dzh_CN%26CH%3Dtencent%26' \
+              'UID%3Dda4b99bf-5e2b-4204-a92f-235474b32c4c&from={}'.format(page)
+        page += 10
+        resp = requests.get(url, headers=headers).json()
+        datas = resp.get('data').get('list')
+        if datas:
+            for data in datas:
+                # 评论人
+                name = data.get('author').get('name')
+                # 游戏时长
+                played_tips = data.get('played_tips')
+                # 评论内容
+                contents = data.get('contents').get('text')
+
+                # 声明一个字典储存数据
+                data_dict = {}
+                data_dict['name'] = name
+                data_dict['played_tips'] = played_tips
+                data_dict['contents'] = contents.replace('<br />', '')
+                data_list.append(data_dict)
+
+                print(data_dict)
+        else:
+            break
+
+
+def main():
+
+    start_spider()
+    # 将数据写入json文件
+    with open('game.json', 'a+', encoding='utf-8') as f:#game.json可改存放路径
+        json.dump(data_list, f, ensure_ascii=False, indent=4)
+    print('json文件写入完成')
+if __name__ == '__main__':
+
+    main()
